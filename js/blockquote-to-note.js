2178a717f887a43b13cde283f65bb1ba0dc07b00
Disembo <zjg2018a@hotmal.com>
2023-08-18 17:33:06 +0800

    Site updated: 2023-08-18 17:33:05

--- js/blockquote-to-note.js
+++ js/blockquote-to-note.js
@@ -1,11 +1,32 @@
 (function () {
 
+    let STYLES = {
+        default: "note-secondary",
+        pf: "note-secondary",
+        proof: "note-secondary",
+        example: "note-info",
+        note: "note-warning",
+    };
+
     let bqs = Array.prototype.slice.call( document.getElementsByTagName("blockquote") );
 
     bqs.forEach(bq => {
+        var quote_type = "default";
+
+        // the first underlined word in the first paragraph
+        var first_words = bq.firstElementChild?.firstElementChild;
+
+        if (first_words?.tagName == 'U') {
+            quote_type = first_words.innerHTML.toLowerCase();
+
+            // in case of `Example 1`
+            quote_type = quote_type.split(" ")[0];
+        }
+
         var note = document.createElement("div");
         note.innerHTML = bq.innerHTML;
-        note.className = "note note-secondary";
+        note.className = "note " + STYLES[quote_type];
+
         bq.replaceWith(note);
     });
 })();
