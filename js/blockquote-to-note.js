6f016b75ca4670816e67c442b76b788f5c79466d
Disembo <zjg2018a@hotmal.com>
2023-08-31 21:00:17 +0800

    Site updated: 2023-08-31 21:00:16

--- js/blockquote-to-note.js
+++ js/blockquote-to-note.js
@@ -1,6 +1,6 @@
 (function () {
 
-    let STYLES = {
+    const STYLES = {
         default: "note-secondary",
         pf: "note-secondary",
         proof: "note-secondary",
@@ -8,22 +8,34 @@
         note: "note-warning",
     };
 
+    function stringEmpty(s) {
+        return s.match(/^[ \n\r\t]*$/);
+    }
+
+    function getTheVeryFirstElementChild(node) {
+        if (!node || !node.firstChild) return;
+        if (node.firstChild === node.firstElementChild)
+            return node.firstChild;
+        if (stringEmpty(node.firstChild.textContent))
+            return node.firstElementChild;
+        return;
+    };
+
     let bqs = Array.prototype.slice.call( document.getElementsByTagName("blockquote") );
 
     bqs.forEach(bq => {
-        var quote_type = "default";
+        let quote_type = "default";
 
         // the first underlined word in the first paragraph
-        var first_words = bq.firstElementChild?.firstElementChild;
+        let first_words = getTheVeryFirstElementChild(getTheVeryFirstElementChild(bq));
 
         if (first_words?.tagName == 'U') {
-            quote_type = first_words.innerHTML.toLowerCase();
+            quote_type = first_words.innerHTML.toLowerCase().split(" ")[0];  // in case of `Example 1`
 
-            // in case of `Example 1`
-            quote_type = quote_type.split(" ")[0];
+            if (!Object.hasOwn(STYLES, quote_type)) quote_type = 'default';
         }
 
-        var note = document.createElement("div");
+        let note = document.createElement("div");
         note.innerHTML = bq.innerHTML;
         note.className = "note " + STYLES[quote_type];
 
